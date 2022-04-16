ec0b7f79b03a3eb7bd879ef8662d3a8da3a46295
Francesco Pallotta <26849744+plt-francesco@users.noreply.github.com>
2022-04-16 12:50:45 +0200

    still not getting a good sphere

--- assignments/A09/models/sphere.cpp
+++ assignments/A09/models/sphere.cpp
@@ -3,16 +3,33 @@ void createSphere(int hCount, int vCount, float r)
     float vStep, hStep, phi, theta, x, y, z, pi = M_PI;
     int k1, k2;
 
-    vStep = pi / vCount;
-    hStep = 2 * pi / hCount;
+    /*
+        A sphere is a circle rotated around a diameter generated
+        by a sphere. Those are the parametric equations for
+        a surface revolution:
+        f(u) * cos(v)
+        f(u) * sin(v)
+        g(u)
+        Where f(u), g(u) are the parametric equations of the rotate curve.
+        For a circle those are:
+        f(u) = r * cos(u)
+        g(u) = r * sin(u)
+        Putting all together:
+        r * cos(u) * cos(v) = r * cos(phi) * cos(theta)
+        r * cos(u) * sin(v) = r * cos(phi) * sin(theta)
+        r * sin(u)          = r * sin(phi)
+    */
 
     M3_vertices.resize(hCount * vCount);
-    for (int i = 0; i <= vCount; i++)
+
+    hStep = 2 * pi / hCount;
+    vStep = pi / vCount;
+    for (int i = 0; i <= vCount; ++i)
     {
         phi = pi / 2 - i * vStep;
-        for (int j = 0; j <= hCount; j++)
+        for (int j = 0; j <= hCount; ++j)
         {
-            theta = j * vStep;
+            theta = j * hStep;
             x = r * cos(phi) * cos(theta);
             y = r * cos(phi) * sin(theta);
             z = r * sin(phi);
@@ -26,49 +43,45 @@ void createSphere(int hCount, int vCount, float r)
     // |  / |
     // | /  |
     // k2--k2+1
+
+    // 2 triangles per horizontal step (hStep), excluding first and last stacks
+    // k1 => k2 => k1+1
     M3_indices.resize(hCount * vCount);
     for (int i = 0; i < vCount; i++)
     {
-        for (int j = 0; j < hCount; j++)
+        k1 = i * (hCount + 1); // beginning of current vertical step (vStep)
+        k2 = k1 + hCount + 1;  // beginning of next vertical step (vStep)
+
+        for (int j = 0; j < hCount; ++j, ++k1, ++k2)
         {
-            k1 = i * hCount + j;
-            k2 = k1 + hCount;
-            M3_indices.push_back(k1);
-            M3_indices.push_back(k2);
-            M3_indices.push_back(k1 + 1);
-            M3_indices.push_back(k2);
-            M3_indices.push_back(k2 + 1);
-            M3_indices.push_back(k1 + 1);
+            // 2 triangles per horizontal step, excluding first and last vertical steps:
+            // k1 => k2 => k1+1
+            if (i != 0)
+            {
+                M3_indices.push_back(k1);
+                M3_indices.push_back(k2);
+                M3_indices.push_back(k1 + 1);
+            }
+
+            // k1+1 => k2 => k2+1
+            if (i != (vCount - 1))
+            {
+                M3_indices.push_back(k1 + 1);
+                M3_indices.push_back(k2);
+                M3_indices.push_back(k2 + 1);
+            }
         }
     }
 }
 
-// possible working variant of indexing:
-// for (int i = 0; i < hCount; i++)
-//     {
-//         for (int j = 0; j < vCount; j++)
-//         {
-//             k1 = i * (hCount + 1) + j;
-//             k2 = k1 + hCount + 1;
-//             for (int j = 0; j < hCount; j++)
-//             {
-//                 if (i != 0)
-//                 {
-//                     M3_indices.push_back(k1);
-//                     M3_indices.push_back(k2);
-//                     M3_indices.push_back(k1 + 1);
-//                 }
-
-//                 // k1+1 => k2 => k2+1
-//                 if (i != (vCount - 1))
-//                 {
-//                     M3_indices.push_back(k1 + 1);
-//                     M3_indices.push_back(k2);
-//                     M3_indices.push_back(k2 + 1);
-//                 }
-
-//                 k1++;
-//                 k2++;
-//             }
-//         }
-//     }
+// for (int j = 0; j < hCount; j++)
+// {
+//     k1 = i * hCount + j;
+//     k2 = k1 + hCount;
+//     M3_indices.push_back(k1);
+//     M3_indices.push_back(k2);
+//     M3_indices.push_back(k1 + 1);
+//     M3_indices.push_back(k2);
+//     M3_indices.push_back(k2 + 1);
+//     M3_indices.push_back(k1 + 1);
+// }
